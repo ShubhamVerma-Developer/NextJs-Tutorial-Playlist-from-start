a2d2608392b7937e9cd52732297fc2096c4e0ff9
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-25 22:32:38 +0530

    Next.Js 14 Tutorial - 2 - Hello World

---
+++ Next.Js 14 Tutorial - 2 - Hello World/hello-world/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container justify-center items-center">
+        <h1 className="bg-red-500 justify-center items-center">Hello World </h1>
+      </div>
+    </main>
+  );
+}
