63f3933cc244560edffa81a108c022507227c8cc
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-27 21:15:36 +0530

    Next.js 14 Tutorial - 10 - Not Found Page

---
+++ Next.js 14 Tutorial - 10 - Not Found Page/not-found/src/app/review/[...rate]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Notfound from "../not-found";
+
+const page = () => {
+  Notfound();
+  return <div>page</div>;
+};
+
+export default page;
---
+++ Next.js 14 Tutorial - 10 - Not Found Page/not-found/src/app/review/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div>
+      <h1>Page Not found</h1>
+      <h2>This page not found customize</h2>
+    </div>
+  );
+};
+
+export default Notfound;
